30f4571589eeaea8722960cdc95effca964c3376
jiieko <jiieko@gmail.com>
2023-01-06 20:33:05 +0700

    add strings.py add def multi to nums.py

--- nums.py
+++ nums.py
@@ -6,9 +6,11 @@ def minus(a, b):
     return a - b
 
 
-text = "new text"
+def multi(a, b):
+    return a * b
 
 
 if __name__ == '__main__':
     print(plus(2, 2))
     print(minus(4, 2))
+    print(multi(3, 3))
